3b12ed85b1a395e107220396ae7b35b0fb704679
suenefuzzo <suenequeiroz@hotmail.com>
2023-05-11 22:29:17 -0300

    feat: users routes

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import "reflect-metadata";
 import "express-async-errors";
 import express, { Application } from "express";
 import { handleErros } from "./error";
+import usersRoutes from "./routes/users.routes";
 
 const app: Application = express();
 app.use(express.json());
+
+app.use("/users", usersRoutes)
 
 app.use(handleErros)
 
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const usersRoutes: Router = Router();
+
+usersRoutes.post("", );
+
+usersRoutes.get("", );
+
+usersRoutes.patch("/:id", );
+
+usersRoutes.delete("/:id", );
+
+export default usersRoutes;

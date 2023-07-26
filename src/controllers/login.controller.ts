f643a5a03c132de335d5cd946a2036dd1b26be1a
suenefuzzo <suenequeiroz@hotmail.com>
2023-07-26 09:48:08 -0300

    first commit

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -4,9 +4,7 @@ import createTokenService from "../services/login/createToken.service";
 
 const createTokenController = async (request: Request, response: Response): Promise<Response> => {
     const loginData: TLoginRequest = request.body;
-
     const token = await createTokenService(loginData);
-
     return response.json({token});
 };
 

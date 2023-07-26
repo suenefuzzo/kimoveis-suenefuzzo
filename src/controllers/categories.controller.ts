e2c84f6c66651586970e74b16b5dbac3c6862cf6
suenefuzzo <suenequeiroz@hotmail.com>
2023-07-26 09:45:21 -0300

    feat: making repository public

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -6,23 +6,18 @@ import listRealEstatesByCategoryService from "../services/categories/listRealEst
 
 const createCategoryController = async (request: Request, response: Response): Promise<Response> => {
     const categoryData: TCategoryRequest = request.body;
-
     const newCategory = await createCategoryService(categoryData)
-
     return response.status(201).json(newCategory)
 }
 
 const listCategoriesController = async (request: Request, response: Response): Promise<Response> => {
     const listCategories: TCategoryResponse[] = await listCategoriesService();
-
     return response.json(listCategories);
 };
 
 const listRealEstatesByCategoryController = async (request: Request, response: Response): Promise<Response> => {
     const categoryId: number = Number(request.params.id);
-
     const listRealEstatesByCategory  = await listRealEstatesByCategoryService(categoryId);
-
     return response.json(listRealEstatesByCategory);
 };
 
